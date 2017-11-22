bdb3b5d9ccce7f17ffff4bbb139c3384f258f477
Carlos Vicens <carlos.vicens.alonso@gmail.com>
2017-11-22 17:52:25 +0100

    Adding script to set roles for lab 4

--- set-roles.sh
+++ set-roles.sh
@@ -2,8 +2,8 @@
 
 oc adm policy add-role-to-group admin portalapp -n portalapp-dev
 oc adm policy add-role-to-group admin portalapp -n portalapp-test
-oc adm policy add-role-to-group admin paymentapp -n portalapp-dev
-oc adm policy add-role-to-group admin paymentapp -n portalapp-test
+oc adm policy add-role-to-group admin paymentapp -n paymentapp-dev
+oc adm policy add-role-to-group admin paymentapp -n paymentapp-test
 oc adm policy add-role-to-group admin ocp-production -n portalapp-prod
 oc adm policy add-role-to-group admin ocp-production -n paymentapp-prod
 oc adm policy add-cluster-role-to-group cluster-admin ocp-platform
